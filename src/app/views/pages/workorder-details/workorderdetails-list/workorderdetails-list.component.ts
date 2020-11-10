addd49d9c55c5fcee66260fd73f6e53b4c28a6b6
سياق لإنتاج المحتوى الرقمي <ceyac.net@gmail.com>
2020-11-10 04:06:11 +0200

    add foremen role to new button on order details list

--- src/app/views/pages/workorder-details/workorderdetails-list/workorderdetails-list.component.ts
+++ src/app/views/pages/workorder-details/workorderdetails-list/workorderdetails-list.component.ts
@@ -4,6 +4,7 @@ import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@ang
 import { MatDialog, MatDialogConfig, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTable, MatTableDataSource } from '@angular/material/table';
+import { AuthService } from 'src/app/core/auth';
 import { WorkOrderDetail } from 'src/app/core/_models/workOrderDetail.model';
 import { WorkorderdetailsService } from 'src/app/core/_services/_service/workorder-details.service';
 import { WorkorderEditComponent } from '../../work-order/workorder-edit/workorder-edit.component';
@@ -17,23 +18,26 @@ import { WorkorderdetailsEditComponent } from '../workorderdetails-edit/workorde
 export class WorkorderdetailsListComponent implements OnInit, AfterViewInit {
 
 
-  displayedDetailColumns: string[] = ['id', 'location','description',  'progress'];
+  displayedDetailColumns: string[] = ['id', 'location', 'description', 'progress'];
   public dataSource = new MatTableDataSource;
   controls: FormArray;
   dataLength: number;
   workerId: number;
+  isForemen: boolean;
 
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChildren(MatTable) _matTables;
 
   constructor(private workorderdetailsService: WorkorderdetailsService,
-    @Inject(MAT_DIALOG_DATA) public data: any, public dialog: MatDialog,) {
+    @Inject(MAT_DIALOG_DATA) public data: any, public dialog: MatDialog,
+    authService: AuthService
+  ) {
 
-
+    this.isForemen = authService.isForemen;
   }
   ngAfterViewInit(): void {
-   
+
     this.dataSource.paginator = this.paginator;
 
   }
@@ -45,7 +49,7 @@ export class WorkorderdetailsListComponent implements OnInit, AfterViewInit {
       this.fillTableData(this.data.recordId);
     }
 
-   
+
 
   }
   fillTableData(workorderId: number) {
@@ -54,20 +58,20 @@ export class WorkorderdetailsListComponent implements OnInit, AfterViewInit {
       .subscribe(result => {
         this.dataLength = result.length;
         this.dataSource.data = result;
-        console.log(this.dataSource.data );
-        const toGroups = this.dataSource.data.map((entity:WorkOrderDetail) => {
+        console.log(this.dataSource.data);
+        const toGroups = this.dataSource.data.map((entity: WorkOrderDetail) => {
           return new FormGroup({
-            id:  new FormControl(entity.id, Validators.required),
-            description: new FormControl(entity.description, Validators.required), 
+            id: new FormControl(entity.id, Validators.required),
+            description: new FormControl(entity.description, Validators.required),
             location: new FormControl(entity.location, Validators.required),
             progress: new FormControl(entity.progress, Validators.required),
-            workOrderId:new FormControl(entity.workOrderId, Validators.required),
-          },{updateOn: "blur"});
+            workOrderId: new FormControl(entity.workOrderId, Validators.required),
+          }, { updateOn: "blur" });
         });
-    
+
         this.controls = new FormArray(toGroups);
-    
-        console.log(this.controls );
+
+        console.log(this.controls);
       },
         (err: HttpErrorResponse) => {
           console.log(err.error);
@@ -79,20 +83,20 @@ export class WorkorderdetailsListComponent implements OnInit, AfterViewInit {
 
   createNewWorkOrderDetails(id: number): WorkOrderDetail {
     if (this.data) {
-      return { id: 10,description:"",location:"",progress:0,workOrderId:this.data.recordId};
+      return { id: 10, description: "", location: "", progress: 0, workOrderId: this.data.recordId };
     }
 
-    return { id: 10,description:"",location:"",progress:0,workOrderId:1};
+    return { id: 10, description: "", location: "", progress: 0, workOrderId: 1 };
   }
 
-  addRow(){
+  addRow() {
     // this.dataSource.data.push(this.createNewWorkOrderDetails(this.dataSource.data.length + 1));
     // this.dataSource.filter = "";
     this.newWorkOrderDetails()
   }
 
 
-  newWorkOrderDetails(){
+  newWorkOrderDetails() {
     const dialogConfig = new MatDialogConfig();
     dialogConfig.disableClose = true;
     dialogConfig.autoFocus = true;
